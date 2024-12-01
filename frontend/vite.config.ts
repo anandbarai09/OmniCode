0430920c6e0a63dc086ce0c6f36f4eb66a886b1e
anandbarai09 <anandbarai09@gmail.com>
2024-12-01 22:00:19 +0530

    removed tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    preact(),
-    tailwindcss()
+    tailwindcss(),
+    preact()
   ],
   server: {
     port: 3000,
